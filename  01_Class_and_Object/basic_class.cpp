9bcaedbcf170a0196b1fde4ff2b7829b1070a0b4
Prashant Kumar <prashantkumar936800@gmail.com>
2026-09-12 23:57:05 +0530

    Update class and object code

---  01_Class_and_Object/basic_class.cpp
+++  01_Class_and_Object/basic_class.cpp
@@ -1,25 +1,51 @@
+// #include<iostream>
+// using namespace std;
+
+// class Student{
+//     public:
+//         string name;
+//         int rollno;
+//         float CGPa;
+// };
+// int main(){
+//     Student s1;
+//     s1.name = "Prashant Kumar";
+//     s1.rollno = 46;
+//     s1.CGPa = 8.7;
+
+//     Student s2;
+//     s2.name = "Rohit Gupta";
+//     s2.rollno = 40;
+//     s2.CGPa = 9.3;
+
+//     cout<<s1.name<<" "<<s1.rollno<<" "<<s1.CGPa<<endl;
+//     cout<<s2.name<<" "<<s2.rollno<<" "<<s2.CGPa<<endl;
+
+//     return 0;
+// }
+
+
 #include<iostream>
 using namespace std;
 
 class Student{
     public:
         string name;
-        int rollno;
-        float CGPa;
+        int age;
+
+    void display(){
+            cout<<name<<endl;
+            cout<<age<<endl;
+    }
+
 };
+
 int main(){
     Student s1;
     s1.name = "Prashant Kumar";
-    s1.rollno = 46;
-    s1.CGPa = 8.7;
+    s1.age = 20;
 
-    Student s2;
-    s2.name = "Rohit Gupta";
-    s2.rollno = 40;
-    s2.CGPa = 9.3;
-
-    cout<<s1.name<<" "<<s1.rollno<<" "<<s1.CGPa<<endl;
-    cout<<s2.name<<" "<<s2.rollno<<" "<<s2.CGPa<<endl;
+    s1.display();
 
     return 0;
 }
