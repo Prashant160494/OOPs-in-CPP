f196b1f9b309b9fc9fe6f43419220c0e7f8e4eb9
Prashant Kumar <prashantkumar936800@gmail.com>
2026-09-13 01:43:17 +0530

    Add Car details

---
+++ 01_Class_and_Object_fixed/CarDetail.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+class Car{
+    public:
+        string name;
+        int price;
+        int seat;
+        string type;
+
+    void display(){
+        cout<<name<<" "<<price<<" "<<seat<<" "<<type<<endl;
+    }    
+};
+int main(){
+    Car c1;
+    c1.name = "Honda Suzuki";
+    c1.price = 600000;
+    c1.seat = 5;
+    c1.type = "Sedan";
+
+    Car c2;
+    c2.name = "Swift Dezire";
+    c2.price = 400000;
+    c2.seat = 5;
+    c2.type = "Sedan";
+    
+    Car c3;
+    c3.name = "Fortuner";
+    c3.price = 3200000;
+    c3.seat = 8;
+    c3.type = "SUV";
+
+    Car c4;
+    c4.name = "Grand Vitara";
+    c4.price = 150000;
+    c4.seat = 7;
+    c4.type = "SUV";
+
+    // cout<<c1.name<<" "<<c1.price<<" "<<c1.seat<<" "<<c1.type<<endl;
+    // cout<<c2.name<<" "<<c2.price<<" "<<c2.seat<<" "<<c2.type<<endl;
+    // cout<<c3.name<<" "<<c3.price<<" "<<c3.seat<<" "<<c3.type<<endl;
+    // cout<<c4.name<<" "<<c4.price<<" "<<c4.seat<<" "<<c4.type<<endl;
+
+    c1.display();
+    c2.display();
+    c3.display();
+    c4git.display();
+
+    return 0;
+}
